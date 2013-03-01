259dbbce4f8d30bdc1a4310879382f37f4ba006e
Gloton <gloton@gmail.com>
2013-03-01 16:24:50 -0300

    2.2 Creando la clase Controller Creando la clase controller en el archivo Controller.php Video 2 14:30 Es una clase abstracta, que apesar que puse el comentario para que es lo que se utiliza, la voy a investigar mas.

--- application/Controller.php
+++ application/Controller.php
@@ -1,1 +1,21 @@
 <?php
+//tiene que ser una clase abstracta para que no pueda ser instanciada
+abstract class Controller
+{
+	/*	este medodo es abstracto que obliga a que todas las clases que 
+		hereden de Controller, implementen el metodo index por obligacion
+		aunque este no tenga codigo.
+		De esta manera, nos aseguramos que siempre haya un metodo index en
+		todos nuestros controladores, que es e metodo que se va a asignar 
+		por defecto en el archivo Request.php 
+		
+        if(!$this->_metodo){
+            $this->_metodo = 'index'; 
+        }
+        cuando no se envie un metodo al controlador, o se envie un metodo
+        por error, ya eso lo va a corregir el bootstrap
+    */ 
+    abstract public function index();
+}
+
+?>
