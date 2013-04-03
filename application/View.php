ede23a1c776a5254c510d51ef4f8c9024a6638cd
Gloton <gloton@gmail.com>
2013-04-02 22:36:53 -0300

    Video 3 - documentando Muy poquita informacion agregue, mas que nada solo estoy tratando de hacerle un buen seguimiento. Luego lo tratare de documentar en forma mas detallada.

--- application/View.php
+++ application/View.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * A diferecia de los controladores que todos que todos deban extender 
+ * A diferecia de los controladores que todos deban extender 
  * desde un controlador principal. En las vistas no es necesario ya que 
  * estas no son instanciadas, pero si necesitamos un objeto que nos maneje 
  * el trabajo con las vistas, y eso lo va a ser la clase View
@@ -20,31 +20,36 @@ class View
 
 		$menu = array(
 				array(
+						//este nos va a llavar a un controlador
 						'id' => 'inicio',
 						'titulo' => 'Inicio',
 						'enlace' => BASE_URL
 				),
 		
 				array(
+						//y este otro nos va a llavar a otro controlador
 						'id' => 'hola',
 						'titulo' => 'Hola',
 						'enlace' => BASE_URL . 'hola'
 				)
 		);
-				
 		# $_layoutParams
 		#con este array se le enviaran la informacion sobre las rutas 
 		#para agregar las rutas absolutas a las carpetas; css, img y js
+		
+		#Cuando le colocamos BASE_URL, no se le incluye la constante DS porque siempre el separador sera slash ej; 'views/layout/'
 		$_layoutParams = array(
 				'ruta_css' => BASE_URL . 'views/layout/' . DEFAULT_LAYOUT . '/css/',
 				'ruta_img' => BASE_URL . 'views/layout/' . DEFAULT_LAYOUT . '/img/',
 				'ruta_js' => BASE_URL . 'views/layout/' . DEFAULT_LAYOUT . '/js/',
 				'menu' => $menu
-		);		
+		);
+		//crearemos una carpeta por cada controlador ej;	controllers/indexController.php views/index	
 		$rutaView = ROOT . 'views' . DS . $this->_controlador . DS . $vista . '.phtml';
 		if(is_readable($rutaView)){
 			//para incluir el header
 			include_once ROOT . 'views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'header.php';
+			//es el contenido de la vista ej;views/index/index.phtml (depende del controlador)
 			include_once $rutaView;
 			//para incluir el footer
 			include_once ROOT . 'views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'footer.php';
